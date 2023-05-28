3211919b8c2f4968068daefec405d120af6c4a66
suptts <134623522+suptts@users.noreply.github.com>
2023-05-28 06:40:20 +0000

    adding scraffold of python project

---
+++ hello.py
@@ -0,0 +1,14 @@
+def more_hello():
+    return "hi"
+
+
+def more_goodbye():
+    return "bye"
+
+
+# This is nonsense code that generate a warning
+# var = 1
+# var = var
+
+# This is a bad syntax
+# foo=
